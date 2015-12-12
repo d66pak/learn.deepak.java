9edcd1d7e9fda0a14dccde49198d0dfd42c84c2f
deepak <rdeepakt@gmail.com>
2015-12-12 09:01:50 +0900

    Intrinsic lock test

---
+++ myjava/src/main/java/learn/deepak/java/concurrent/IntrinsicLocks.java
@@ -0,0 +1,7 @@
+package learn.deepak.java.concurrent;
+
+/**
+ * Created by deepak on 11/12/15.
+ */
+public class IntrinsicLocks {
+}
---
+++ myjava/src/test/java/learn/deepak/java/concurrent/IntrinsicLocksTest.java
@@ -0,0 +1,10 @@
+package learn.deepak.java.concurrent;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by deepak on 11/12/15.
+ */
+public class IntrinsicLocksTest {
+
+}
