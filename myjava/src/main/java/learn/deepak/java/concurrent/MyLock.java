fbb6b5806f56d8a98cc9aee99394dbcb64512936
deepak <rdeepakt@gmail.com>
2015-12-12 19:46:28 +0900

    Simple lock implementation

--- myjava/src/main/java/learn/deepak/java/concurrent/MyLock.java
+++ myjava/src/main/java/learn/deepak/java/concurrent/MyLock.java
@@ -1,7 +1,44 @@
 package learn.deepak.java.concurrent;
 
 /**
- * Created by deepak on 12/12/15.
+ * Non reentrant
+ * Non fair
  */
 public class MyLock {
+
+    private boolean locked = false;
+    private Thread lockingThread;
+    private final Object monitor = new Object();
+
+    public void lock() throws InterruptedException {
+
+        synchronized (monitor) {
+            while (locked) {
+                monitor.wait();
+            }
+
+            /**
+             * Lock has become available now or
+             * lock was already available
+             */
+            System.out.println("Lock granted to : " + Thread.currentThread());
+            locked = true;
+            lockingThread = Thread.currentThread();
+        }
+    }
+
+    public void unlock() {
+
+        synchronized (monitor) {
+            if (lockingThread != Thread.currentThread()) {
+                throw new IllegalMonitorStateException("Thread trying to unlock is not the owner of this lock!");
+            }
+
+            System.out.println("Unlocked by : " + Thread.currentThread());
+            locked = false;
+            lockingThread = null;
+            monitor.notify();
+        }
+    }
+
 }
--- myjava/src/test/java/learn/deepak/java/concurrent/MyLockTest.java
+++ myjava/src/test/java/learn/deepak/java/concurrent/MyLockTest.java
@@ -1,10 +1,61 @@
 package learn.deepak.java.concurrent;
 
+import org.junit.Test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.Assert.*;
 
-/**
- * Created by deepak on 12/12/15.
- */
 public class MyLockTest {
 
+    @Test
+    public void testMyLock() throws InterruptedException {
+
+        MyLock lock = new MyLock();
+
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+
+        executor.submit(() -> {
+            System.out.println("Thread 1 : acquiring lock...");
+            try {
+                lock.lock();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Thread 1 : lock is acquired, entering critical section");
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Thread 1 : exiting critical section, releasing lock");
+            lock.unlock();
+        });
+
+        executor.submit(() -> {
+            System.out.println("Thread 2 : trying to acquire lock...");
+            try {
+                lock.lock();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Thread 2 : lock is acquired, entering critical section");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Thread 2 : exiting critical section, releasing lock");
+            lock.unlock();
+
+        });
+
+        // Need to wait before closing
+        Thread.sleep(30000);
+        
+        executor.shutdownNow();
+        executor.awaitTermination(10, TimeUnit.SECONDS);
+    }
 }
