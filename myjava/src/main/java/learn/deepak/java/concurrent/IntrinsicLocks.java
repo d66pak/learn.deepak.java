20ab5c8d0d85a3ed8ccbc757ada27bcc2ce2d746
deepak <rdeepakt@gmail.com>
2015-12-12 09:02:46 +0900

    Intrinsic lock test

--- myjava/src/main/java/learn/deepak/java/concurrent/IntrinsicLocks.java
+++ myjava/src/main/java/learn/deepak/java/concurrent/IntrinsicLocks.java
@@ -1,7 +1,39 @@
 package learn.deepak.java.concurrent;
 
+import java.util.concurrent.CountDownLatch;
+
 /**
  * Created by deepak on 11/12/15.
  */
 public class IntrinsicLocks {
+
+    private final CountDownLatch _latch;
+    private int _counter;
+
+    public IntrinsicLocks(CountDownLatch latch) {
+        _latch = latch;
+    }
+
+    public synchronized int increment() {
+
+        System.out.println("Incrementing counter...");
+        if (_counter == Integer.MAX_VALUE) {
+            throw new IllegalStateException("Reached MAX integer limit");
+        }
+        _counter += 1;
+
+        try {
+            _latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return _counter;
+    }
+
+    public synchronized int getCounter() {
+
+        System.out.println("Returning counter...");
+        return _counter;
+    }
 }
--- myjava/src/test/java/learn/deepak/java/concurrent/IntrinsicLocksTest.java
+++ myjava/src/test/java/learn/deepak/java/concurrent/IntrinsicLocksTest.java
@@ -1,10 +1,49 @@
 package learn.deepak.java.concurrent;
 
+import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by deepak on 11/12/15.
  */
 public class IntrinsicLocksTest {
+
+    @Test
+    public void testIntrinsicLocks() throws InterruptedException {
+
+        CountDownLatch latch = new CountDownLatch(1);
+        IntrinsicLocks il = new IntrinsicLocks(latch);
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+
+        executor.submit(() -> {
+            System.out.println("Calling increment...");
+            int counter = il.increment();
+            System.out.println("Incremented to : " + counter);
+        });
+
+        executor.submit(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Reading counter...");
+                int counter = il.getCounter();
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                latch.countDown();
+                System.out.println("Counter value is : " + counter);
+            }
+        });
+
+        executor.shutdownNow();
+        executor.awaitTermination(30, TimeUnit.SECONDS);
+    }
 
 }
